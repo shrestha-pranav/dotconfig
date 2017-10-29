637532d049350a18a3d6bf938ee6695a97669c56
Pranav Shrestha <Pranav Shrestha>
2017-10-29 00:15:14 -0400

    Fixed setup script

--- setup.sh
+++ setup.sh
@@ -2,3 +2,8 @@
 echo "source ~/.config/.bash_profile" >> ~/.bash_profile
 echo "source ~/.config/.vimrc" >> ~/.vimrc
 echo "source-file ~/.config/.tmux.conf" >> ~/.tmux.conf
+cp ~/.config/.minttyrc ~/.minttyrc
+mkdir -p ~/.backup
+mkdir -p ~/.backup/.backups
+mkdir -p ~/.backup/.undofiles
+mkdir -p ~/.backup/.swapfiles
